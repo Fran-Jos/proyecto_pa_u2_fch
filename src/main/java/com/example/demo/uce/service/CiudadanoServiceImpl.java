b74818110f655c8598f7d568814bbb6fe6b920cf
Fran-Jos <fjchanataxi@uce.edu.ec>
2023-01-23 10:03:25 -0500

    deber oneTOone

---
+++ src/main/java/com/example/demo/uce/service/CiudadanoServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.uce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.uce.modelo.Ciudadano;
+import com.example.demo.uce.repository.ICiudadanoRepo;
+
+@Service
+public class CiudadanoServiceImpl implements ICiudadanoService {
+	
+	@Autowired
+	private ICiudadanoRepo ciudadanoRepo;
+
+	@Override
+	public void agregar(Ciudadano ciudadano) {
+		// TODO Auto-generated method stub
+		
+		this.ciudadanoRepo.insertar(ciudadano);
+	}
+
+	@Override
+	public void modificar(Ciudadano ciudadano) {
+		// TODO Auto-generated method stub
+		
+		this.ciudadanoRepo.actualizar(ciudadano);
+	}
+
+	@Override
+	public Ciudadano buscar(Integer id) {
+		// TODO Auto-generated method stub
+		return this.ciudadanoRepo.buscar(id);
+	}
+
+	@Override
+	public void eliminar(Integer id) {
+		// TODO Auto-generated method stub
+		
+		this.ciudadanoRepo.eliminar(id);
+	}
+
+}
